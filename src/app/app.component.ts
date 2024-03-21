b7fa738b80e0100be3b2c15496db9f130a300df3
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-03-22 02:29:44 +0300

    FoodOrgan components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from "./comps/layout/footer/footer.component";
+import {BlogComponent} from "./comps/layout/blog/blog.component";
+import {CategoryComponent} from "./comps/layout/category/category.component";
+import {HeaderComponent} from "./comps/layout/header/header.component";
+import {HeroComponent} from "./comps/layout/hero/hero.component";
+import {BurgerComponent} from "./comps/layout/burger/burger.component";
+import {FeaturedComponent} from "./comps/layout/featured/featured.component";
+import {BannerComponent} from "./comps/layout/banner/banner.component";
+import {LatestProductComponent} from "./comps/layout/latest-product/latest-product.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, BlogComponent, CategoryComponent, HeaderComponent, HeroComponent, BurgerComponent, FeaturedComponent, BannerComponent, LatestProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/comps/layout/latest-product/latest-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './latest-product.component.html',
+  styleUrl: './latest-product.component.css'
+})
+export class LatestProductComponent {
+
+}
